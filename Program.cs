060af5b63f251246ad52fa886fc6ef4536075164
Matheus Santon <matheussantong@gmail.com>
2024-01-21 18:04:14 -0300

    Init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Mail;
+
+using (SmtpClient client = new SmtpClient("mailslurper"))
+{
+    client.Port = 2500;
+
+    using (MailMessage message = new MailMessage())
+    {
+        message.From = new MailAddress("sender@example.com");
+        message.To.Add(new MailAddress("recipient@example.com"));
+
+        message.Subject = "Test Email";
+        message.Body = "This is a test email.";
+        try
+        {
+            client.Send(message);
+            Console.WriteLine("Email sent successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error sending email: {ex.Message}");
+            Console.WriteLine(ex);
+        }
+    }
+}
